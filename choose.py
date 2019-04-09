21d760ec2cea14f4cd6cb1bfd7c8ceb3c90c7abe
Lasers67 <b16026@students.iitmandi.ac.in>
2019-04-10 00:11:53 +0530

    Final Commit

---
+++ choose.py
@@ -0,0 +1,2 @@
+def get_card_from_user(player,player_1_name,player_2_name,player_3_name,player_1_score,player_2_score,player_3_score,player_1_round_score,player_2_round_score,player_3_round_score,player_1_passed_cards,player_2_passed_cards,player_3_passed_cards,cardsforhand):
+	return player.hand[0]
---
+++ get_3cards.py
@@ -0,0 +1,3 @@
+def get_3_cards(player,to_pass):
+	# to_pass can be left , right or across
+	return player.hand[0],player.hand[1],player.hand[2]
