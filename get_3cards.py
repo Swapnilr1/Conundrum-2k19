6f2b522ec36d91a0b3d4d31c46927cf16d44ce27
rishi-s8 <33762894+rishi-s8@users.noreply.github.com>
2019-04-17 00:07:05 +0530

    make suits array

--- get_3cards.py
+++ get_3cards.py
@@ -1,3 +1,7 @@
 def get_3_init(player,to_pass):
 	# to_pass can be left , right or across
+	suits = {'C': [], 'H': [], 'D':[], 'S':[]}
+	for hand in player.hand:
+		suits[hand.suit].append(hand.rank)
+	print(suits)
 	return player.hand[0],player.hand[1],player.hand[2]
