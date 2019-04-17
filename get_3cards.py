3add9e61ef622d1f521a465bb3d83b650f850cbf
Manvi07 <b17092@students.iitmandi.ac.in>
2019-04-17 15:16:48 +0530

    Card object modified

--- get_3cards.py
+++ get_3cards.py
@@ -1,4 +1,4 @@
-from objects import *
+import objects
 
 def getdeck(player):
 	decks = {'C': [], 'H': [], 'D':[], 'S':[]}
@@ -7,63 +7,66 @@ def getdeck(player):
 	return decks
 
 def get_3_init(player,to_pass):
-	return player.hand[0],player.hand[1],player.hand[2]
 	# to_pass can be left , right or across
 	suits = ['H', 'C', 'D', 'S']
 	decks = {'C': [], 'H': [], 'D':[], 'S':[]}
 
-	to_pass = []
-	taken_to_pass = 0
+	Pass = []
+	taken_Pass = 0
 	for hand in player.hand:
 		decks[hand.suit].append(hand.rank)
 
-	
 	if 12 in decks['S']:
 		if 13 in decks['S']:
-			to_pass.extend([Card('S', 12),Card('S', 13)])	#Change all extend and append to this format
-			taken_to_pass += 2
+			Pass.extend([objects.Card('S', 12),objects.Card('S', 13)])	#Change all extend and append to this format
+			taken_Pass += 2
 		elif 14 in decks['S']:
-			to_pass.extend([12,14])
-			taken_to_pass += 2
+			Pass.extend([object.Card('S', 12),object.Cards('S', 14)])
+			taken_Pass += 2
 		elif 13 in decks['S'] and 14 in decks['S']:
-			to_pass.extend([12,13,14])
-			taken_to_pass += 3
+			Pass.extend([objects.Card('S', 12),objects.Card('S', 13), objects.Card('S', 14)])
+			taken_Pass += 3
 		elif len(decks['S']) < 3:
-			to_pass.extend(decks['S'])
-			taken_to_pass += len(decks['S'])
+			for i in decks['S']:
+				Pass.append(objects.Card('S', i))
+			taken_Pass += len(decks['S'])
 
 
 	if 12 not in decks['S']:
 		if 13 in decks['S']:
-			to_pass.append(13)
-			taken_to_pass += 1
+			Pass.append(objects.Card('S', 13))
+			taken_Pass += 1
 		if 14 in decks['S']:
-			to_pass.append(14)
-			taken_to_pass += 1
+			Pass.append(objects.Card('S', 14))
+			taken_Pass += 1
 		for suit in suits:
 			deck = decks[suit]
-			if len(deck) <= 3 - taken_to_pass:
-				to_pass.extend(deck)
-				taken_to_pass += len(deck)
+			if len(deck) <= 3 - taken_Pass:
+				for i in deck:
+					Pass.append(objects.Card(suit,i))
+				taken_Pass += len(deck)
 
 		if 2 in decks['C']:
-			to_pass.append(2)
-			taken_to_pass += 1
+			Pass.append(objects.Card('C', 2))
+			taken_Pass += 1
 
-		heart = list(set(deck['H']))
+		heart = list(set(decks['H']))
 		for x in range(len(heart)-1, 0, -1):
-			to_pass.append(heart[x])
-			taken_to_pass += 1
-			if taken_to_pass == 3:
+			Pass.append(objects.Card('H', heart[x]))
+			taken_Pass += 1
+			if taken_Pass >= 3:
 				break
 
-		for x in range(14,2, -1):
+		for x in range(14, 2, -1):
 			for s in ['D', 'C']:
-				to_pass.append(Card(s,x))
-				taken_to_pass += 1
-				if taken_to_pass ==3:
-					break
-			if taken_to_pass ==3:
+				if x in decks[s]:
+					Pass.append(objects.Card(s, x))
+					taken_Pass += 1
+					if taken_Pass >=3:
+						break
+			if taken_Pass >=3:
 				break
-
-	return to_pass[0],to_pass[1],to_pass[2]
+	print(len(Pass))
+	# for i in Pass:
+	# 	print(i.suit, i.rank)
+	return Pass[0],Pass[1],Pass[2]
