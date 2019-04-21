963947a7d70554703ef860e7a8e08b9792061e01
rishi-s8 <33762894+rishi-s8@users.noreply.github.com>
2019-04-21 16:38:11 +0530

    Fix Wrong Card

--- choose.py
+++ choose.py
@@ -1,5 +1,4 @@
 from random import randint
-
 def getindex(player, suit, rank):
 	index = 0
 	for hand in player.hand:
@@ -15,31 +14,40 @@ def getdeck(player):
 
 def get_card_from_user(player,player_1_name,player_2_name,player_3_name,player_1_score,player_2_score,player_3_score,player_1_round_score,player_2_round_score,player_3_round_score,player_1_passed_cards,player_2_passed_cards,player_3_passed_cards,cardsforhand):
 	# function returns a card from your hand for the current round.
+	heart_played = False
+	print("Player_1 Passed Cards")
+	i = (len(player_1_passed_cards)//13) * 13
+
+	while i<len(player_1_passed_cards) and heart_played == False:
+		if player_1_passed_cards[i].suit =='H':
+			heart_played = True
+		i+=1
+	print("Player_2 Passed Cards")
+	i = (len(player_2_passed_cards)//13) * 13
+
+	while i<len(player_2_passed_cards) and heart_played == False:
+		if player_2_passed_cards[i].suit =='H':
+			heart_played = True
+			break
+		i+=1
+
+	print("Player_3 Passed Cards")
+	i = (len(player_3_passed_cards)//13) * 13
+
+	while i<len(player_3_passed_cards) and heart_played == False:
+		if player_3_passed_cards[i].suit =='H':
+			heart_played = True
+			break
+		i+=1
+
 	decks = getdeck(player)
 	if 2 in decks['C']:
 		return player.hand[getindex(player, 'C', 2)]
 	if len(cardsforhand)==0: #My turn is first
-		heart=False
-		for i in cardsforhand:
-			if i.suit == 'H':
-				heart = True
-				break
-		for i in player_1_passed_cards:
-			if i.suit == 'H':
-				heart = True
-				break
-		for i in player_2_passed_cards:
-			if i.suit == 'H':
-				heart = True
-				break
-		for i in player_3_passed_cards:
-			if i.suit == 'H':
-				heart = True
-				break
 		available_suits = []
 		if (len(decks['C'])>0):
 			available_suits.append('C')
-		if (heart and len(decks['H'])>0):
+		if (len(decks['H'])>0) and heart_played:
 			available_suits.append('H')
 		if (len(decks['D'])>0):
 			available_suits.append('D')
@@ -47,7 +55,7 @@ def get_card_from_user(player,player_1_name,player_2_name,player_3_name,player_1
 		if (len(available_suits)==0): #only Spades remaining
 			sortedspades = sorted(decks['S'])
 			return player.hand[getindex(player, 'S', decks['S'][0])]
-		
+
 		suitn = randint(1, len(available_suits)) - 1
 		suit = available_suits[suitn]
 		rank = decks[suit][randint(0, len(decks[suit]) -1)]
@@ -84,12 +92,12 @@ def get_card_from_user(player,player_1_name,player_2_name,player_3_name,player_1
 			if 14 in decks['S']:
 				card_to_play = ('S', 14)
 			elif len(decks['H']) != 0:
-				card_to_play = ('H', sorted(decks['H'])[-1])	
+				card_to_play = ('H', sorted(decks['H'])[-1])
 			elif len(decks['D']) != 0:
 				card_to_play = ('D', sorted(decks['D'])[-1])
 			elif len(decks['C']) != 0:
 				card_to_play = ('C', sorted(decks['C'])[-1])
 			elif len(decks['S']) != 0:
 				card_to_play = ('S', sorted(decks['S'])[-1])
-			
+
 		return player.hand[getindex(player, card_to_play[0], card_to_play[1])]
